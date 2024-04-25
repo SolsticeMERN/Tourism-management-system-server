ffb9995c72903f4236278f69779ae7278fb624a3
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-04-26 01:46:40 +0600

    mongodb install

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port= process.env.PORT || 3000;
@@ -6,6 +7,39 @@ const port= process.env.PORT || 3000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// tourismManagement
+// znMlNZaTu8rfuTkL
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.0rmazcr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
